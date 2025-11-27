9d91973e3789efd7866a4d872129606dbbb7c02c
nickonyi <nickoluoch1130@gmail.com>
2025-11-27 18:41:59 +0300

    creating the view template using ejs

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import path from "path";
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// app.js
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", indexRouter);
+
+app.listen(PORT, () => {
+  console.log(`listening at port:${PORT}`);
+});
