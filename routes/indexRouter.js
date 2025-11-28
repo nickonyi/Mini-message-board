81734eeff6c1d7fc41b179883f5f8a08c1fd2150
nickonyi <nickoluoch1130@gmail.com>
2025-11-28 12:58:24 +0300

    fixed theview bug issue

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -13,6 +13,46 @@ const messages = [
     user: "Charles",
     added: new Date(),
   },
+  {
+    text: "How’s everyone doing today?",
+    user: "Lena",
+    added: new Date(),
+  },
+  {
+    text: "Just finished working on my project!",
+    user: "Sam",
+    added: new Date(),
+  },
+  {
+    text: "Anyone up for a quick chat?",
+    user: "Derrick",
+    added: new Date(),
+  },
+  {
+    text: "The weather is amazing today!",
+    user: "Nora",
+    added: new Date(),
+  },
+  {
+    text: "Learning Express is fun!",
+    user: "Tasha",
+    added: new Date(),
+  },
+  {
+    text: "Good morning everyone!",
+    user: "Brian",
+    added: new Date(),
+  },
+  {
+    text: "Coffee break time ☕",
+    user: "Elena",
+    added: new Date(),
+  },
+  {
+    text: "Just joined the chat!",
+    user: "Kyle",
+    added: new Date(),
+  },
 ];
 
 indexRouter.get("/", (req, res) => {
