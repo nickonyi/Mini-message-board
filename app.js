45d069809ba1f88961815bbf5d4d9f41205791a8
nickonyi <nickoluoch1130@gmail.com>
2025-12-01 14:56:31 +0300

    added the controller for thr router

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import { pathToFileURL } from "url";
+import path from "path";
 
 const app = express();
 
 dotenv.config();
+
+const __fileName = pathToFileURL(import.meta.url);
+const __dirName = path.dirname(__fileName);
+console.log(__fileName);
 
 const PORT = process.env.PORT;
 
